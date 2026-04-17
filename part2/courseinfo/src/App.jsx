e4086d9a771a42672e82ae0f507d57f36ea224ff
Andre <prince000515@gmail.com>
2026-04-18 01:46:11 +0200

    part2: courseinfo part10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,51 +1,6 @@
+import Course from './components/Course'
 
 const Title = ({ title }) => <h1>{title}</h1>
-
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(item => (
-        <Part key={item.id} name={item.name} exercises={item.exercises}/>
-    ))}
-    </div>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
-
-const Total = ({ course }) => {
-  const items = course.parts
-  const sum = items.reduce((sum, item) => sum + item.exercises, 0)
-
-  return (
-    <h4>total of {sum} exercises</h4>
-  )
-}
-
-const Course = ({ courses }) => {
-
-  if (courses.length === 0) return null
-
-  return (
-    <div>
-      {courses.map(course => (
-        <div key={course.id}>
-          <Header course={course.name} />
-          <Content course={course} />
-          <Total course={course} /> 
-        </div>
-    ))}
-    </div>
-  )
-}
 
 const App = () => {
 
